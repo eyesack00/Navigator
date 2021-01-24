c85d66083b74a8bf46c5939a9d9c0bbcad2aeb28
eyesack00 <77690918+eyesack00@users.noreply.github.com>
2021-01-24 12:20:17 -0600

    Update start.py

--- start.py
+++ start.py
@@ -1,10 +1,10 @@
 import os
 
-
+os.system("cd ~")
 choose = input("Would you like to replace the code?")
 if choose:
-    os.system("cd Desktop")
+    os.system("cd home/pi/Desktop")
     os.system("rm -r Reader")
     os.system("git clone https://github.com/eyesack00/Reader")
-os.system("cd Desktop/Reader")
+os.system("cd home/pi/Desktop/Reader")
 os.system("python3 LoadRead.py")
